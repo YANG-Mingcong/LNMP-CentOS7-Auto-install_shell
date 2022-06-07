a6516cab4495aaf056a0cd35c75dcfb28ff28b4c
YANG Mingcong <49844552+YANG-Mingcong@users.noreply.github.com>
2022-06-08 01:12:20 +0200

    Create autoInstall.sh

---
+++ autoInstall.sh
@@ -0,0 +1,29 @@
+#run in root user
+yum -y update;
+yum -y upgrade;
+
+systemctl stop firewalld;
+
+
+yum -y install nginx;
+systemctl start nginx;
+systemctl enable nginx;
+systemctl stop nginx;
+
+yum -y install mariadb mariadb-server;
+systemctl start mariadb;
+systemctl enable mariadb;
+
+
+yum install \
+https://repo.ius.io/ius-release-el7.rpm \
+https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm;
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm;
+yum -y install php74-devel php74-cli.x86_64 php74-common.x86_64 php74-gd.x86_64 php74-ldap.x86_64 php74-mbstring.x86_64 php74-pdo.x86_64 php74-mysqlnd php74-fpm php74-opcache php74-pecl-redis php74-xml;
+systemctl start php-fpm;
+systemctl enable php-fpm;
+systemctl stop php-fpm;
+
+systemctl daemon-reload;
+
+
